d8a3c1070cfb61e9f1dd22ca0865c542bce18b31
MariePrr <mariefcperier@gmail.com>
2023-09-29 09:53:34 +0200

    Envoi pour sauvegarder sur github

---
+++ scripts/main.js
@@ -0,0 +1,20 @@
+/*********************************************************************************
+ * 
+ * Point d'entrée, c'est lui qui intialise le jeu et lance la boucle de jeu. 
+ * 
+ *********************************************************************************/
+
+lancerJeu()
+
+
+// let inputEcriture = document.getElementById("inputEcriture");
+// let btnValiderMot = document.getElementById("btnValiderMot");
+
+// let divZoneProposition = document.querySelector(".ZoneProposition");
+// let spanScore = document.querySelector(".zoneScore span");
+
+// let listeInputRadio = document.querySelectorAll(".optionSource input"); 
+
+
+
+
